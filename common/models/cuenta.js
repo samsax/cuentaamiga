52561a9e98d40ef78209998e71c7e9221f9e2151
Sa <samuelsaxofon@gmail.com>
2016-09-08 09:31:39 -0500

    historial cuentas

--- common/models/cuenta.js
+++ common/models/cuenta.js
@@ -1,3 +1,18 @@
 module.exports = function(Cuenta) {
+	Cuenta.getCuentas = function(id1,id2, cb) {
+    Cuenta.find({where:{or:[{'usuarioPago':id1}, {'usuarioDebe':id2},{'usuarioPago':id2}, {'usuarioDebe':id1}]}}, function (err, instance) {
+       cb(null, instance);
+    });
+  }
 
+  Cuenta.remoteMethod (
+        'getCuentas',
+        {
+          http: {path: '/getcuentas', verb: 'get'},
+          accepts:[{arg: 'id1', type: 'string'},
+          			{arg: 'id2', type: 'string'} 
+          			 		],
+          returns: {arg: 'id', type: 'string'}
+        }
+    );
 };
